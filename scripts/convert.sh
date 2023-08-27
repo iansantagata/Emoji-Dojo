1df5da9960ee8537f150b1198be343660bc453b7
Ian Santagata <7070901+iansantagata@users.noreply.github.com>
2023-08-27 19:03:20 -0400

    Minor cleanup with better error handling, adjusted options, and a way to skip prompts if requested

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -6,7 +6,7 @@
 #
 # ./convert.sh --help
 # ./convert.sh --from jpg --to png .
-# ./convert.sh --from=jpg --to=png --delete
+# ./convert.sh --from=jpg --to=png --in-place
 # ./convert.sh -f jpg -t png path/to/directory
 # ./convert.sh -t png -s path/to/file.jpg
 
@@ -30,24 +30,29 @@ display_help()
     echo "Note: $SCRIPT_NAME requires Image Magick to be installed."
     echo
     echo "OPTIONS:"
-    echo "  -d, --delete                       (OPTIONAL) Delete old files after conversion to new type;"
-    echo "                                                Functions as an in-place FILE conversion"
     echo "  -f, --from FORMAT, --from=FORMAT   (OPTIONAL) Image FORMAT to convert images from;"
     echo "                                                REQUIRED when FILE is provided as a directory;"
     echo "                                                Must be omitted when FILE is provided as a regular file"
     echo "  -h, --help                         (OPTIONAL) Display this help and exit"
-    echo "  -s, --skip-prompt                  (OPTIONAL) Skip user confirmation prompts for FILE conversion and deletion"
+    echo "  -i, --in-place                     (OPTIONAL) Remove old FILEs after conversion to new type;"
+    echo "                                                Simulates an in-place FILE conversion"
+    echo "  -s, --skip-prompt                  (OPTIONAL) Skip user confirmation prompt before operating on FILEs"
     echo "  -t, --to FORMAT, --to=FORMAT       (REQUIRED) Image FORMAT to convert images to"
+}
+
+echo_error()
+{
+    echo "$1" >&2
 }
 
 set_from_type()
 {
     if [ -z "$1" ]; then
-        echo "Error: No value provided for option: '-f'" >&2
+        echo_error "Error: No value provided for option: '-f'"
         display_help
         exit 1
     elif [ -n "$CONVERT_FROM" ]; then
-        echo "Error: Duplicate options provided for: '-f'" >&2
+        echo_error "Error: Duplicate options provided for: '-f'"
         display_help
         exit 1
     fi
@@ -57,11 +62,11 @@ set_from_type()
 set_to_type()
 {
     if [ -z "$1" ]; then
-        echo "Error: No value provided for option: '-t'" >&2
+        echo_error "Error: No value provided for option: '-t'"
         display_help
         exit 1
     elif [ -n "$CONVERT_TO" ]; then
-        echo "Error: Duplicate options provided for: '-t'" >&2
+        echo_error "Error: Duplicate options provided for: '-t'"
         display_help
         exit 1
     fi
@@ -71,11 +76,11 @@ set_to_type()
 parse_file_name()
 {
     if [ -z "$1" ]; then
-        echo "Error: No FILE provided" >&2
+        echo_error "Error: No FILE provided"
         display_help
         exit 1
     elif [ ! -e "$1" ]; then
-        echo "Error: FILE '$1' does not exist" >&2
+        echo_error "Error: FILE '$1' does not exist"
         exit 1
     elif [ -d "$1" ]; then
         DIRECTORY=$1
@@ -83,9 +88,9 @@ parse_file_name()
         FILE="$(basename "$1")"
         DIRECTORY="$(dirname "$1")"
     else
-        echo "Error: Unrecognized type for FILE: '$1'" >&2
-        echo >&2
-        echo "File type must be a regular file or a directory" >&2
+        echo_error "Error: Unrecognized type for FILE: '$1'"
+        echo_error
+        echo_error "File type must be a regular file or a directory"
         display_help
         exit 1
     fi
@@ -94,19 +99,19 @@ parse_file_name()
 validate_inputs()
 {
     if [ -z "$CONVERT_FROM" ] && [ -z "$FILE" ]; then
-        echo "Error: No value provided for required option: '-f'" >&2
+        echo_error "Error: No value provided for required option: '-f'"
         display_help
         exit 1
     fi
 
     if [ -n "$CONVERT_FROM" ] && [ -n "$FILE" ]; then
-        echo "Error: Cannot use non-directory FILE '$FILE' with option: '-f'" >&2
+        echo_error "Error: Cannot use non-directory FILE '$FILE' with option: '-f'"
         display_help
         exit 1
     fi
 
     if [ -z "$CONVERT_TO" ]; then
-        echo "Error: No value provided for required option: '-t'" >&2
+        echo_error "Error: No value provided for required option: '-t'"
         display_help
         exit 1
     fi
@@ -122,17 +127,17 @@ set_default_inputs()
 verify_image_magick_installed()
 {
     if ! command -v magick >/dev/null 2>&1; then
-        echo "Error: Image Magick is not installed locally" >&2
-        echo >&2
-        echo "$SCRIPT_NAME will not work properly without Image Magick" >&2
-        echo "See install instructions for Image Magick here: https://imagemagick.org" >&2
+        echo_error "Error: Image Magick is not installed locally"
+        echo_error
+        echo_error "$SCRIPT_NAME will not work properly without Image Magick"
+        echo_error "See install instructions for Image Magick here: https://imagemagick.org"
         exit 1
     fi
 }
 
 ask_for_confirmation()
 {
-    if [ "$SKIP_CONVERSATION_PROMPT" != "true" ]; then
+    if [ "$SKIP_CONFIRMATION_PROMPT" != "true" ]; then
         while true; do
             echo
             read -p "Do you wish to continue? (Y/N): " RESPONSE
@@ -145,19 +150,31 @@ ask_for_confirmation()
     fi
 }
 
-list_files()
+check_files()
 {
     if [ -n "$FILE" ]; then
-        find $DIRECTORY -type f -iname $FILE
+        FILE_LIST="$(find $DIRECTORY -type f -iname $FILE)"
+        FILE_COUNT="$(find $DIRECTORY -type f -iname $FILE | wc -l)"
     else
-        find $DIRECTORY -type f -iname "*.$CONVERT_FROM"
+        FILE_LIST="$(find $DIRECTORY -type f -iname "*.$CONVERT_FROM")"
+        FILE_COUNT="$(find $DIRECTORY -type f -iname "*.$CONVERT_FROM" | wc -l)"
+    fi
+
+    if [ -z "$FILE_COUNT" ] || [ "$FILE_COUNT" -eq 0 ]; then
+        echo_error "Error: No files found!"
+        exit 1
     fi
 }
 
 convert_files()
 {
+    check_files
+
     echo "Ready to convert files to '$CONVERT_TO' format:"
-    list_files
+    echo "$FILE_LIST"
+    echo
+    echo "Total file count: $FILE_COUNT"
+
     ask_for_confirmation
 
     echo "Starting file conversion..."
@@ -171,21 +188,17 @@ convert_files()
     echo "File conversion complete!"
 }
 
-delete_files()
+delete_preconversion_files()
 {
-    if [ "$DELETE_AFTER_CONVERSION" = "true" ]; then
-        echo "Ready to delete files with the previous format before conversion:"
-        list_files
-        ask_for_confirmation
-
-        echo "Starting file deletion..."
+    if [ "$IN_PLACE_CONVERSION" = "true" ]; then
+        echo "Starting previous format file deletion..."
 
         if [ -n "$FILE" ]; then
-            rm -f "$FILE"
+            rm -f $FILE
         elif [ "$DIRECTORY" = "." ]; then
             rm -f *.$CONVERT_FROM
         else
-            rm -f "$DIRECTORY/*.$CONVERT_FROM"
+            rm -f $DIRECTORY/*.$CONVERT_FROM
         fi
 
         echo "File deletion complete!"
@@ -202,17 +215,17 @@ fi
 while [ "$#" -gt 0 ]; do
     case $1 in
         # Short and long options with any possible values separated by spaces (alphabetical order)
-        -d|--delete) DELETE_AFTER_CONVERSION="true"; shift;;
         -f|--from) set_from_type "$2"; shift 2;;
         -h|--help) display_help; exit 0;;
-        -s|--skip-prompt) SKIP_CONVERSATION_PROMPT="true"; shift;;
+        -i|--in-place) IN_PLACE_CONVERSION="true"; shift;;
+        -s|--skip-prompt) SKIP_CONFIRMATION_PROMPT="true"; shift;;
         -t|--to) set_to_type "$2"; shift 2;;
 
         # Long options with any possible values separated by equal signs (alphabetical order)
         --from=*) set_from_type "${1#*=}"; shift;;
         --to=*) set_to_type "${1#*=}"; shift;;
 
-        -*|--*) echo "Error: Unknown option: $1" >&2; display_help; exit 1;;
+        -*|--*) echo_error "Error: Unknown option: '$1'"; display_help; exit 1;;
         *) parse_file_name "$1"; shift;;
     esac
 done
@@ -222,7 +235,7 @@ verify_image_magick_installed
 validate_inputs
 set_default_inputs
 convert_files
-delete_files
+delete_preconversion_files
 
 echo "Execution complete!"
 exit 0
