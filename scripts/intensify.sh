0ffd15315d7aeca70569baedab269f7457adf1a8
Ian Santagata <7070901+iansantagata@users.noreply.github.com>
2023-08-27 19:35:32 -0400

    Copy basic script structure to intensify script

---
+++ scripts/intensify.sh
@@ -0,0 +1,110 @@
+#! /bin/bash
+#
+# This script creates an animated GIF of an input image that appears to shake within the image's frame.
+#
+# Example Usages:
+#
+# ./intensify.sh --help
+
+set -e
+SCRIPT_NAME=$0
+
+display_help()
+{
+    echo "Usage: $SCRIPT_NAME [OPTIONS ...] FILE"
+    echo "Create an animated GIF of an input image in FILE that appears to shake within the image's frame."
+    echo
+    echo "OPTIONS:"
+    echo "  -h, --help   (OPTIONAL) Display this help and exit"
+}
+
+echo_error()
+{
+    echo "$1" >&2
+}
+
+parse_file_name()
+{
+    if [ -z "$1" ]; then
+        echo_error "Error: No FILE provided"
+        display_help
+        exit 1
+    fi
+
+    if [ ! -e "$1" ]; then
+        echo_error "Error: FILE '$1' does not exist"
+        exit 1
+    fi
+
+    if [ -f "$1" ] && [ -n "$FILE" ]; then
+        echo_error "Error: More than one FILE was provided: '$FILE' and '$1'"
+        display_help
+        exit 1
+    fi
+
+    # Determine what type of file type this is (regular file is allowed)
+    if [ -f "$1" ]; then
+        FILE="$(basename "$1")"
+        DIRECTORY="$(dirname "$1")"
+    else
+        echo_error "Error: Unrecognized type for FILE: '$1'"
+        echo_error
+        echo_error "File type must be a regular file"
+        display_help
+        exit 1
+    fi
+}
+
+validate_inputs()
+{
+    # TODO - Implement
+    echo
+}
+
+set_default_inputs()
+{
+    # TODO - Implement
+    echo
+}
+
+verify_image_magick_installed()
+{
+    if ! command -v magick >/dev/null 2>&1; then
+        echo_error "Error: Image Magick is not installed locally"
+        echo_error
+        echo_error "$SCRIPT_NAME will not work properly without Image Magick"
+        echo_error "See install instructions for Image Magick here: https://imagemagick.org"
+        exit 1
+    fi
+}
+
+intensify_file()
+{
+    # TODO - Implement
+    echo
+}
+
+# Show help if no inputs are provided
+if [ "$#" -eq 0 ]; then
+    display_help
+    exit 0
+fi
+
+# Iteratively process inputs
+while [ "$#" -gt 0 ]; do
+    case $1 in
+        # Short and long options with any possible values separated by spaces (alphabetical order)
+        -h|--help) display_help; exit 0;;
+        -*|--*) echo_error "Error: Unknown option: '$1'"; display_help; exit 1;;
+        *) parse_file_name "$1"; shift;;
+    esac
+done
+
+# Execute desired logic based on inputs
+verify_image_magick_installed
+validate_inputs
+set_default_inputs
+intensify_file
+
+echo "Execution complete!"
+exit 0
